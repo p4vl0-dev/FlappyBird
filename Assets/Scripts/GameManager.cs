bb4032acc34e5a4bc4b16d71e23980168ef1fcb7
p4vl0 <p4vl0.dev@gmail.com>
2023-01-19 15:07:12 +0300

    Added PauseMenu and changed some mechanics

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,10 @@ public class GameManager: MonoBehaviour
 {
     public GameObject ScoreWindow;
     public GameObject LoseWindow;
+    public GameObject PauseMenu;
+    //HideInInspector - не показывает в инспекторе данное поле
+    [HideInInspector] public bool isGameLosed = false; //Поле, которое показывает - проиграна игра или нет. Помогает работать PauseMenu
+    
     public static GameManager instanceManager; //Для использования не статичных методов в других скриптах(классах)
 
     private void Start()
@@ -33,10 +37,24 @@ public class GameManager: MonoBehaviour
 
     public void Lose()
     {   
+        isGameLosed = true; //Инициализирует проигрыш и не дает паузе открываться
         Time.timeScale = 0;
         Spawner.SetDefaults();
         PipeMove.SetDefaults();
         LoseWindow.SetActive(true);
         ScoreWindow.SetActive(false);
+        PauseMenu.SetActive(false);
+    }
+
+    public void Pause() //Для паузы в игре
+    {
+        Time.timeScale = 0;
+        PauseMenu.SetActive(true);
+    }
+    
+    public void Continue() //Для продолжения игры
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,36 @@
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour{
-    public Rigidbody2D Player;
+    public Rigidbody2D Player; //Компонент rigibody, объекта Player в инспекторе
+    private bool isGamePaused = false; //Проверка на паузу в игре
+
     private void Update()
     {
-        if((Input.GetKeyDown(KeyCode.Space)) || (Input.GetKeyDown(KeyCode.W)) || (Input.GetKeyDown(KeyCode.UpArrow)))
+        if(Input.GetKeyDown(KeyCode.Escape)&&GameManager.instanceManager.isGameLosed == false) //Не дает нажиматься кнопке ESC во время проигрыша
+        {   
+            isGamePaused = !isGamePaused; //Переключатель паузы
+            if(isGamePaused == true) //Запускает паузу
+            {
+                GameManager.instanceManager.Pause(); 
+            }
+            if(isGamePaused == false) //Отключает меню паузы
+            {
+                GameManager.instanceManager.Continue();
+            }
+        }
+
+        if(((Input.GetKeyDown(KeyCode.Space)) || (Input.GetKeyDown(KeyCode.W)) || (Input.GetKeyDown(KeyCode.UpArrow)))&&(GameManager.instanceManager.isGameLosed == false)) //если игра не проиграна, то нажатия нужных клавиш проходит - иначе они отключаются
         {
             Player.velocity = new Vector2(0f, 0f); //Скорость по осям, чтобы отталкиваться в любой точке при любой скорости падения - я убираю скорость по y на 0. (Velocity - сопротивление воздуха)
-            Player.AddForce(Vector2.up * 235);
+            Player.AddForce(Vector2.up * 235); //Прыжок
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionEnter2D(Collision2D other) //При соприкосновении с коллайдером объектов
     {
         if(other.gameObject.CompareTag("pipePart"))
         {
-            GameManager.instanceManager.Lose();
+            GameManager.instanceManager.Lose(); //Инициализирует проигрыш
         }
     }
 }
